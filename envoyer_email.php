c166076fbbea357eaa5f345b09f91d930001c980
Théo Engelaere <theo.engelaere.etu@univ-lille.fr>
2023-10-23 18:05:17 +0200

    emailV1

--- envoyer_email.php
+++ envoyer_email.php
@@ -1,5 +1,21 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nom = $_POST["nom"];
+    $message = $_POST["message"];
+    
+    $destinataire = "theo.engelaere.etu@univ-lille.com"; // Remplacez par votre propre adresse e-mail
+    $sujet = "Nouveau message de contact de $nom";
+    $entete = "De : $nom";
+
+    // Envoi de l'e-mail
+    mail($destinataire, $sujet, $message, $entete);
+
+    // Rediriger l'utilisateur vers une page de confirmation
+    header("Location: confirmation.html");
+}
+/*
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
     $email = $_POST["email"];
     $object = $_POST["object"];
@@ -11,4 +27,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     mail($dest, $subject, $message, $head);
 }
+?>
+*/
 ?>
