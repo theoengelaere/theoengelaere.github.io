20fa9b7dfd3167084f7c2938c0a8fb3e9df02c10
Théo Engelaere <theo.engelaere.etu@univ-lille.fr>
2023-10-23 17:41:36 +0200

    contact

---
+++ envoyer_email.php
@@ -0,0 +1,14 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $object = $_POST["object"];
+    $message = $_POST["message"];
+    
+    $dest = "theo.engelaere.etu@univ-lille.com"
+    $subject = "Nouveau message de contact de $nom : $object";
+    $head = "De : $nom, $email";
+
+    mail($dest, $subject, $message, $head);
+}
+?>
