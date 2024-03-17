ae99bdc5470b298ff9cb47a2729756d81fe2b37e
Théo Engelaere <theo.engelaere.etu@univ-lille.fr>
2024-03-17 14:34:22 +0100

    rectif mauvais code

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ navLinks.forEach(link => {
 });
 
 function linkStyleUpdate() {
-    var scrollPosition = window.scrollY;
+    const scrollPosition = window.scrollY;
 
     if (scrollPosition < 75) {
         header.style.width = "90%";
@@ -52,16 +52,14 @@ function linkStyleUpdate() {
     let idx = 0;
     const section = document.getElementById("presentation");
     if (section) idx = -1;
-    var sectionTop;
-    var sectionHeight;
 
     sections.forEach(section => {
         if (idx == -1) { idx++; return; }
         if (!navLinks[idx]) return;
 
         if (navLinks[idx].matches(':hover')) return;
-        sectionTop = section.offsetTop - 100;
-        sectionHeight = section.clientHeight;
+        const sectionTop = section.offsetTop - 100;
+        const sectionHeight = section.clientHeight;
         if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
             navLinks[idx].style.color = "rgba(240,110,110,1)";
             navLinks[idx].style.textDecoration = "underline";
@@ -76,28 +74,32 @@ function linkStyleUpdate() {
 window.addEventListener("scroll", function () {
     linkStyleUpdate();
 });
+
 function scrollToTop() {
     document.documentElement.scrollTop = 0;
 }
+
 function displayMobileMenu() {
     vMobileMenu = true;
     menuIcon.style.display = "none";
     menu.style.display = "flex";
 }
+
 function closeMobileMenu() {
     vMobileMenu = false;
     if (window.length > 1100) return;
     menuIcon.style.display = "block";
     menu.style.display = "none";
 }
+
 function scrollToMainSection(sectionID) {
     const section = document.getElementById(sectionID);
     if (section) section.scrollIntoView({ behavior: 'smooth' });
 }
+
 function redirect(page, section) {
 
-    console.log(page, section);
-    var link = document.createElement('a');
+    const link = document.createElement('a');
     link.href = "./" + page;
     link.click();
     setTimeout(function () {
@@ -111,8 +113,8 @@ function openLinkedIn() {
     window.open("https://www.linkedin.com/in/theo-engelaere", '_blank');
 }
 function downloadCV() {
-    var pdfURL = './download/CV_Theo_Engelaere_2023.pdf';
-    var link = document.createElement('a');
+    const pdfURL = './download/CV_Theo_Engelaere_2023.pdf';
+    const link = document.createElement('a');
     link.href = pdfURL;
     link.download = 'CV_Theo_Engelaere_2023.pdf';
     link.target = '_blank';
